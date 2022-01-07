765fbe76bc6f067322c56359c3e29811e83d367f
jdwhitaker <justinwhitaker@protonmail.com>
2022-01-07 08:22:57 -0500

    tabs to spaces

--- broken-authentication/src/server.js
+++ broken-authentication/src/server.js
@@ -8,37 +8,37 @@ const port = 8080
 const app = express()
 app.use(bodyParser.json())
 app.use(cookieSession({
-	name: 'session',
-	secret: 'secret-key',
-	maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    name: 'session',
+    secret: 'secret-key',
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
 
 async function authenticate(username, password){
-	const entry = datastore.getUserData(username);
+    const entry = datastore.getUserData(username);
     if(!(entry.password === password)){
-		return false;
-	}
+        return false;
+    }
     return true;
 }
 
 app.post('/authn', (req, res) => {
-	console.error('POST /authn');
-	const username = req.body.userId;
+    console.error('POST /authn');
+    const username = req.body.userId;
     const password = req.body.password;
     // Failure to await result of promise
-	const authenticated = authenticate(username, password);
+    const authenticated = authenticate(username, password);
     // Javascript's weak typing results in the promise 
     // being implicitly converted to a boolean. 
     if(!authenticated){
-		res.sendStatus(401);
+        res.sendStatus(401);
         return;
     }
     // Since a promise is not null, it has a value of true.
-	req.session.userId = req.body.userId;
-	res.sendStatus(200);
+    req.session.userId = req.body.userId;
+    res.sendStatus(200);
     return;
 })
 
 app.listen(port, () => {
-	console.log(`listening at http://localhost:${port}`)
+    console.log(`listening at http://localhost:${port}`)
 })
